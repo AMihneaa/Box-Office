de2c18c2a357e138effd5adb8a56113646958690
Mihnea <asmihnea5@gmail.com>
2022-12-01 03:08:25 +0200

    am creat navbar-ul si componentele pentru fiecare pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+//Import Components
+import Navs from './Components/Navs';
+
+//Import Pages Component
+import Home from './Pages/Home';
+import Starred from './Pages/Starred';
+
 function App() {
   return (
-    <Switch>
-      <Route exact={true} path={'/'}>
-        This is home page
-      </Route>
+    <div>
+      <Navs />
 
-      <Route exact={true} path={'/starred'}>
-        This is starred page
-      </Route>
+      <Switch>
+        <Route exact={true} path={'/'}>
+          <Home />
+        </Route>
 
-      <Route>Err 404</Route>
-    </Switch>
+        <Route exact={true} path={'/starred'}>
+          <Starred />
+        </Route>
+
+        <Route>
+          <div>Eror 404 : Page not found!</div>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
