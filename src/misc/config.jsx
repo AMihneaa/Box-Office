07fa70b851e6f78193ae23f2c23b0ae933488bef
Mihnea <asmihnea5@gmail.com>
2022-12-01 19:45:13 +0200

    displayed show result and small fetch refractor

---
+++ src/misc/config.jsx
@@ -0,0 +1,9 @@
+const API_BASE_URL = 'https://api.tvmaze.com';
+
+export async function apiGet(queryString) {
+  const response = await fetch(`${API_BASE_URL}${queryString}`).then(raw => {
+    return raw.json();
+  });
+
+  return response;
+}
