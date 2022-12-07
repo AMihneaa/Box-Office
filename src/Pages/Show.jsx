bcbc036b350a35febaa3593feba1f038f1c0d098
Mihnea <asmihnea5@gmail.com>
2022-12-07 23:49:50 +0200

    Am adaugat State ul de Loading si Error pentru pagina de Show

--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -8,16 +8,42 @@ const Show = () => {
   const { id } = useParams();
 
   const [show, SetShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  let isMounted = true;
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      SetShow(results);
-    });
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        setTimeout(() => {
+          if (isMounted) {
+            SetShow(results);
+            setIsLoading(false);
+          }
+        }, 200);
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
   }, [id]);
 
   console.log(show);
 
-  return <div>This is Show Page, more info</div>;
+  if (isLoading) {
+    return <div>Data is being LOADING</div>;
+  }
+
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
+
+  return () => {
+    isMounted = false;
+  };
 };
 
 export default Show;
